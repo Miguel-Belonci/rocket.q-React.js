d78cd5846306d537d9f40d4219f3bce996fbb3c6
Miguel Belonci <belonciarcanjo@gmail.com>
2025-12-08 11:19:53 -0300

    Start to create users model

---
+++ backend/src/models/Users.js
@@ -0,0 +1,36 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../config/database.js";
+import bcrypt from "bcryptjs";
+
+const Users = sequelize.define(
+  "Users",
+  {
+    id: { type: DataTypes.INTEGER, primarykey: true, autoIncrement: true },
+    email: { type: DataTypes.TEXT, allowNull: false },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [1, 255],
+      },
+    },
+  },
+  {
+    tableName: "users",
+    hooks: {
+      beforeCreate: async (users) => {
+        if (users.password) {
+          const salt = await bcrypt.genSalt(10);
+          users.password = await bcrypt.hash(users.password, salt);
+        }
+      },
+    },
+  }
+);
+
+Users.prototype.checkPassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+export default Users;
--- frontend/src/components/Login/login.jsx
+++ frontend/src/components/Login/login.jsx
@@ -1,13 +1,23 @@
 import "./login.css";
+import { useState } from "react";
+
+const [inputPassword, setInputPassword] = useState();
+const [inputEmail, setInputEmail] = useState();
 
 function Login() {
   return (
     <div className="login-container">
       <div className="login-box">
-        <h2>Login</h2>
+        <header>
+          <a href="/" id="logo">
+            <img src="/images/logo.svg" alt="Rocket-Q logo" />
+          </a>
 
-        <input placeholder="Email" type="email" />
-        <input placeholder="Senha" type="password" />
+          <h2>Login</h2>
+        </header>
+
+        <input placeholder="Email" type="email" onChange={() => setInputEmail(e.target.value)}/>
+        <input placeholder="Senha" type="password" onChange={() => setInputPassword(e.target.value)}/>
 
         <a href="">Esqueceu a senha?</a>
 
