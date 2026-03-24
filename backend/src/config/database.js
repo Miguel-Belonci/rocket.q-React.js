97c566bd556bf8893c8a808f9404a40e9742f043
Miguel Belonci <belonciarcanjo@gmail.com>
2026-03-24 16:02:01 -0300

    Change sqlite to postgresql

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,10 +1,17 @@
+import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 
-// Initialize Sequelize with SQLite
+dotenv.config();
+
+// Initialize Sequelize with PostgeSQL
 export const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
-  logging: console.log, // Set to false to disable SQL query logging
+  dialect: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  logging: false,
   define: {
     timestamps: true,
     underscored: false,
